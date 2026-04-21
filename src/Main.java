3bda68e855ff099ab900e74d2c31a6b1cfa7d459
Miguel-Gil-Moral <mgilm2324@politecnics.barcelona>
2026-04-21 12:57:21 +0200

    Hazme el commit porfa

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Miguel Gil Moral
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola soy Bartolomeu sant de la cruz");
+    }
+}
